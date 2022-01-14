de6d97fc7db8c9ddb35827d3201f7455c910a44e
PetarWho <petarpenev02@gmail.com>
2022-01-14 22:39:21 +0200

    Update

--- JS Advanced/Exercise/Syntax, Functions and Statements - Exercise/PreviousDay.js
+++ JS Advanced/Exercise/Syntax, Functions and Statements - Exercise/PreviousDay.js
@@ -1,5 +1,6 @@
 function previousDay(year, month, day){
         let dt = new Date(year,month,day);
         dt.setDate(dt.getDate()-1);
-        console.log(`${dt.getFullYear()}-${dt.getMonth()}-${dt.getDate()}`);
+        console.log(`${dt.getFullYear()}-${dt.getMonth()+1}-${dt.getDate()}`);
+        // getMonth() is ZERO-BASED!!!
 }
