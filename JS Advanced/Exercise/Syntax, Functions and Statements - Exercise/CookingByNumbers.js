4acadfef50e5bfac6b3ba5b4701f95603a3d4b98
PetarWho <petarpenev02@gmail.com>
2022-01-14 23:17:13 +0200

    Update

--- JS Advanced/Exercise/Syntax, Functions and Statements - Exercise/CookingByNumbers.js
+++ JS Advanced/Exercise/Syntax, Functions and Statements - Exercise/CookingByNumbers.js
@@ -1,49 +1,16 @@
 function cooking(num, operationA,operationB,operationC,operationD,operationE){
 
     num = Number(num);
+    let arrOfOperations = [operationA,operationB,operationC,operationD,operationE];
 
-    switch(operationA){
-        case 'chop': num/=2; break;
-        case 'dice': num = Math.sqrt(num); break;
-        case 'spice': num+=1; break;
-        case 'bake': num*=3; break;
-        case 'fillet': num-=0.2*num; break;
+    for(let i = 0; i<5;i++){
+        switch(arrOfOperations[i]){
+            case 'chop': num/=2; break;
+            case 'dice': num = Math.sqrt(num); break;
+            case 'spice': num+=1; break;
+            case 'bake': num*=3; break;
+            case 'fillet': num-=0.2*num; break;
+        }
+        console.log(num);
     }
-    console.log(num);
-
-    switch(operationB){
-        case 'chop': num/=2; break;
-        case 'dice': num = Math.sqrt(num); break;
-        case 'spice': num+=1; break;
-        case 'bake': num*=3; break;
-        case 'fillet': num-=0.2*num; break;
-    }
-    console.log(num);
-
-    switch(operationC){
-        case 'chop': num/=2; break;
-        case 'dice': num = Math.sqrt(num); break;
-        case 'spice': num+=1; break;
-        case 'bake': num*=3; break;
-        case 'fillet': num-=0.2*num; break;
-    }
-    console.log(num);
-
-    switch(operationD){
-        case 'chop': num/=2; break;
-        case 'dice': num = Math.sqrt(num); break;
-        case 'spice': num+=1; break;
-        case 'bake': num*=3; break;
-        case 'fillet': num-=0.2*num; break;
-    }
-    console.log(num);
-
-    switch(operationE){
-        case 'chop': num/=2; break;
-        case 'dice': num = Math.sqrt(num); break;
-        case 'spice': num+=1; break;
-        case 'bake': num*=3; break;
-        case 'fillet': num-=0.2*num; break;
-    }
-    console.log(num);
 }
